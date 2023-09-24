0f3b692aec56b4db7d5626d2c334595dd65cb8e6
daegyeom <hyunbin9341@gmail.com>
2023-09-25 00:17:50 +0900

    bubblesort optimization, (if sorting complete, stop iterating)

--- bubblesort.js
+++ bubblesort.js
@@ -1,12 +1,16 @@
 function bubblesort(arr) {
-    const swap = (arr, idx1, idx2) => {
+    const swap = (arr, idx1, idx2, swapped) => {
         [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+        swapped = true;
+        return swapped;
     };
     for(let i = arr.length; i>0; i--){
+        let swapped = false;
         for(let j = 0; j < i-1; j++) {
-            if(arr[j]>arr[j+1]) swap(arr, j, j+1);
+            if(arr[j]>arr[j+1]) swapped = swap(arr, j, j+1, swapped);
         }
+        if(!swapped)  break;
     }
     return arr;
 }
-console.log(bubblesort([5,4,3,2,1]));
+bubblesort([10,9,8,1,2]);
