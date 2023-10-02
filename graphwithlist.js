e905593ff0a6f047ad0df18bc76951fefc53b076
daegyeom <hyunbin9341@gmail.com>
2023-10-02 21:37:06 +0900

    graphwithlist breathfirst using iteration(queue)

--- graphwithlist.js
+++ graphwithlist.js
@@ -15,9 +15,10 @@ class Stack {
         if(!this.head) {
             this.head = newNode;
             this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = this.tail.next;
         }
-        this.tail.next = newNode;
-        this.tail = this.tail.next;
         this.length++;
         return this;
     }
@@ -39,6 +40,38 @@ class Stack {
         }
         this.length--;
         return poppedNode.value;// vertex value
+    }
+}
+class Queue {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    enqueue(value) {
+        let newNode = new Node(value);
+        if(!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = this.tail.next;
+        }
+        this.length++;
+        return this;
+    }
+    dequeue() {
+        if(!this.head)  return undefined;
+        let oldFirst = this.head;
+        if(this.head === this.tail) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.head = this.head.next;
+            oldFirst.next = null;
+        }
+        this.length--;
+        return oldFirst.value;
     }
 }
 // Undirected Graph with adjacencyList
@@ -68,12 +101,12 @@ class Graph {
         delete this.adjacencyList[vertex];
     }
     depthFirstIterative(start) {
-        if(!this.adjacencyList[start]) return; // 아예 없는 vertex 입력시 종료
         const result = [];
         const visited = {};
         const stack = new Stack();
         const adjacencyList = this.adjacencyList;
         function dfs(vertex) {
+            if(!adjacencyList[vertex]) return null; // 아예 없는 vertex 입력시 종료
             visited[vertex] = true;
             stack.push(vertex);
             let currVertex;
@@ -88,31 +121,62 @@ class Graph {
                 });   
             }
         }
-        dfs(start);
+        let isVertexExist = dfs(start);
+        if(isVertexExist === null)  return undefined;
         // 마저 돌지 못한 vertex도 다시 dfs 호출해줘야 함
-        for(let v in this.adjacencyList) {
-            if(!visited[v]) dfs(v);
-        }
+        // for(let v in this.adjacencyList) {
+        //     if(!visited[v]) dfs(v);
+        // }
         return result;
     }
     depthFirstRecursive(start) {
-        if(!this.adjacencyList[start]) return; // 아예 없는 vertex 입력시 종료
         const result = [];
         const visited = {};
         const adjacencyList = this.adjacencyList;
         function dfs(vertex) {
+            if(!adjacencyList[vertex]) return null; // 아예 없는 vertex 입력시 종료
             visited[vertex] = true;
             result.push(vertex);
             adjacencyList[vertex].forEach(neighbor => {
                 if(!visited[neighbor])  dfs(neighbor);
             });
         }
-        dfs(start);
-
+        let isVertexExist = dfs(start);
+        if(isVertexExist === null)  return undefined;
         // 마저 돌지 못한 vertex도 다시 dfs 호출해줘야 함
-        for(let v in this.adjacencyList) {
-            if(!visited[v]) dfs(v);
+        // for(let v in this.adjacencyList) {
+        //     if(!visited[v]) dfs(v);
+        // }
+        return result;
+    }
+    breathFirst(start) {
+        const result = [];
+        const visited = {};
+        const queue = new Queue();
+        const adjacencyList = this.adjacencyList;
+        function bfs(vertex) {
+            if(!adjacencyList[vertex])    return null;
+            visited[vertex] = true;
+            queue.enqueue(vertex);
+            let currVertex;
+            while(queue.length > 0) {
+                currVertex = queue.dequeue();
+                result.push(currVertex);
+                adjacencyList[currVertex].forEach(neighbor =>{
+                // adjacencyList[currVertex].slice().reverse().forEach(neighbor =>{
+                    if(!visited[neighbor]) {
+                        visited[neighbor] = true;
+                        queue.enqueue(neighbor);
+                    }
+                });
+            }
         }
+        let isVertexExist = bfs(start);
+        if(isVertexExist === null)  return undefined;
+        // 마저 돌지 못한 vertex도 다시 bfs 호출해줘야 함
+        // for(let v in this.adjacencyList) {
+        //     if(!visited[v]) bfs(v);
+        // }
         return result;
     }
 }
@@ -131,8 +195,9 @@ graph.addEdge("C","E");
 graph.addEdge("D","E");
 graph.addEdge("D","F");
 graph.addEdge("E","F");
-console.log(graph.depthFirstRecursive("A"));    // [ 'A', 'B', 'D', 'E', 'C', 'F' ]
-console.log(graph.depthFirstIterative("A"));    // [ 'A', 'B', 'D', 'E', 'C', 'F' ]
+// console.log(graph.depthFirstRecursive("A"));    // [ 'A', 'B', 'D', 'E', 'C', 'F' ]
+// console.log(graph.depthFirstIterative("A"));    // [ 'A', 'C', 'E', 'F', 'D', 'B' ]
+// console.log(graph.breathFirst("A"));    // [ 'A', 'B', 'C', 'D', 'E', 'F' ]
 
 //      A
 //   /    \      
